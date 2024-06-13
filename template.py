e5396339bcaaa71d57fa02bc8fa0787a729f041d
Syed Tauhid Ullah Shah <Syed.tauhidullahshah@ucalgary.ca>
2024-06-13 14:05:32 -0600

    Update template.py

--- template.py
+++ template.py
@@ -4,7 +4,7 @@ from pathlib import Path # take care of the path
 print(Path("a\b\c.txt"))
 
 list_of_files=[
-    ".github/workflows",
+    ".github/workflows"/.gitkeep",
     "src/__init__.py",
     "src/components/__init__.py", # pipeline, collection of different components
     "src/components/data_ingestion.py",
