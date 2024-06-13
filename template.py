a24306ed0f16c4bbb8996abe1dfa6b5a56dc7a8a
Syed Tauhid Ullah Shah <Syed.tauhidullahshah@ucalgary.ca>
2024-06-13 15:32:53 -0600

    Files and folders created

--- template.py
+++ template.py
@@ -1,12 +1,16 @@
 import os
-from pathlib import Path # take care of the path
+from pathlib import Path  # take care of the path
+import logging
 
-print(Path("a\b\c.txt"))
+# Setup basic logging configuration
+logging.basicConfig(level=logging.INFO)
 
-list_of_files=[
-    ".github/workflows"/.gitkeep",
+print(Path(r"a\b\c.txt"))
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
     "src/__init__.py",
-    "src/components/__init__.py", # pipeline, collection of different components
+    "src/components/__init__.py",  # pipeline, collection of different components
     "src/components/data_ingestion.py",
     "src/components/data_transformation.py",
     "src/components/model_training.py",
@@ -17,7 +21,7 @@ list_of_files=[
     "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
-    "src/exception/exceptions"
+    "src/exception/exceptions",
     "test/unit/__init__.py",
     "test/integration/__init__.py",
     "init_setup.sh",
@@ -28,17 +32,15 @@ list_of_files=[
     "pyproject.toml",
     "tox.ini",
     "experiment/experiments.ipynb"
-    
-    for filepath in list_of_files:
+]
+
+for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    if filedir != "":
+    if filedir:
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Created directory: {filedir}" for file:{filename})
+        logging.info(f"Created directory: {filedir} for file: {filename}")
 
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
         with open(filepath, "w") as f:
             pass
-    
-    
-]
