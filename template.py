f1237ed900ca33abb05d3c4af9c7173a0344e97d
Syed Tauhid Ullah Shah <Syed.tauhidullahshah@ucalgary.ca>
2024-06-13 13:45:44 -0600

    Update template.py

--- template.py
+++ template.py
@@ -5,11 +5,11 @@ print(Path("a\b\c.txt"))
 
 list_of_files=[
     ".github/workflows",
-    ".src/__init__.py",
-    ".src/components/__init__.py", # pipeline, collection of different components
-    ".src/components/data_ingestion.py",
-    ".src/components/data_transformation.py",
-    ".src/components/model_training.py",
-    ".src/components/model_evaluation.py"
+    "src/__init__.py",
+    "src/components/__init__.py", # pipeline, collection of different components
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_training.py",
+    "src/components/model_evaluation.py"
     
 ]
