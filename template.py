99f29eaae7792a81bb053da768fbf2c6f63d0d51
Syed Tauhid Ullah Shah <Syed.tauhidullahshah@ucalgary.ca>
2024-06-13 13:45:05 -0600

    Update template.py

--- template.py
+++ template.py
@@ -9,6 +9,7 @@ list_of_files=[
     ".src/components/__init__.py", # pipeline, collection of different components
     ".src/components/data_ingestion.py",
     ".src/components/data_transformation.py",
-    ".src/components/model_training.py"
+    ".src/components/model_training.py",
+    ".src/components/model_evaluation.py"
     
 ]
