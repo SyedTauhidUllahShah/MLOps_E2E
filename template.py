963ed71aa1d3a52271309137e7e26c420b2ac36d
Syed Tauhid Ullah Shah <Syed.tauhidullahshah@ucalgary.ca>
2024-06-13 14:04:10 -0600

    Update template.py

--- template.py
+++ template.py
@@ -16,6 +16,7 @@ list_of_files=[
     "src/pipeline/prediction_pipeline.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
+    "src/exception/exceptions"
     "test/unit/__init__.py",
     "test/integration/__init__.py",
     "init_setup.sh",
